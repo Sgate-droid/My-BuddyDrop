4e794ebee81264f5ed31b43f42faf7deeb12221d
jimmymangara20 <jimmymangara20@gmail.com>
2025-06-25 23:51:21 +0300

    recent update

--- config/db.config.js
+++ config/db.config.js
@@ -6,5 +6,6 @@ dotenv.config();
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
   logging: Boolean(process.env.DB_LOGGING)
 });
